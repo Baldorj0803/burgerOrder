f5b48a5d8d39bfd39981bc269007c1f3fe033526
BaldorjZ <balaabalaa338@gmail.com>
2021-01-23 18:54:05 +0800

    Updates

---
+++ precache-manifest.47370146dc66651a5af63b7b78d2bcfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e7447ea39d8baa42313f8d46770f82",
+    "url": "/burgerOrder/index.html"
+  },
+  {
+    "revision": "8bcfa496c5ece6e85050",
+    "url": "/burgerOrder/static/css/main.4cc521cc.chunk.css"
+  },
+  {
+    "revision": "d6c7196ec10a276d17a5",
+    "url": "/burgerOrder/static/js/2.5ee8e7b8.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/burgerOrder/static/js/2.5ee8e7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcfa496c5ece6e85050",
+    "url": "/burgerOrder/static/js/main.8dbcfadf.chunk.js"
+  },
+  {
+    "revision": "c2493d91d441f70c62e6",
+    "url": "/burgerOrder/static/js/runtime-main.53b63d73.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerOrder/static/media/burger-logo.b8503d26.png"
+  }
+]);
